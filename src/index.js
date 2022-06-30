bd01a762d4a47f4832d3ff43d776dc0f2ead355c
TedEngineer <tedvu184@gmail.com>
2022-06-30 23:18:04 +1000

    Add webpack to bundle javascript

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,7 +20,17 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
+// Services take app as a parameter
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+// Function takes service as a parameter
+onAuthStateChanged(auth, (user) => {
+  if (user !== null) {
+    console.log("logged in!");
+  } else {
+    console.log("No user");
+  }
+});
